6b97031cf67326424d49136908f3ff8ac50ef3c7
Carsten Schlote <schlote@vahanus.net>
2013-05-03 23:35:58 +0200

    [initial] Added files from Mercurial repository
    Signed-off-by: Carsten Schlote <schlote@vahanus.net>

---
+++ MediaInfoDotNet/Models/ChapterStream.cs
@@ -0,0 +1,46 @@
+﻿/******************************************************************************
+ * MediaInfo.NET - A fast, easy-to-use .NET wrapper for MediaInfo.
+ * Use at your own risk, under the same license as MediaInfo itself.
+ * Copyright (C) 2012 Charles N. Burns
+ * 
+ ******************************************************************************
+ *
+ * ChapterStream.cs
+ * 
+ * Presents information and functionality specific to a chapter stream.
+ * 
+ ******************************************************************************
+ */
+
+using System;
+using MediaInfoLib;
+using MediaInfoDotNet.Models;
+
+namespace MediaInfoDotNet.Models
+{
+	///<summary>Represents a single chapter stream.</summary>
+	public sealed class ChapterStream : Media
+	{
+		readonly MultiStreamCommon streamCommon;
+
+		///<summary>ChapterStream constructor.</summary>
+		///<param name="mediaInfo">A MediaInfo object.</param>
+		///<param name="id">The MediaInfo ID for this chapter stream.</param>
+		public ChapterStream(MediaInfo mediaInfo, int id)
+			: base(mediaInfo, id) {
+			kind = StreamKind.Chapters;
+			streamCommon = new MultiStreamCommon(mediaInfo, kind, id);
+		}
+
+		#region AllStreamsCommon
+		///<summary>The format or container of this file or stream.</summary>
+		public string format { get { return streamCommon.format; } }
+
+		///<summary>The title of this stream.</summary>
+		public string title { get { return streamCommon.title; } }
+
+		///<summary>This stream's globally unique ID (GUID).</summary>
+		public string uniqueId { get { return streamCommon.uniqueId; } }
+		#endregion
+	}
+}
