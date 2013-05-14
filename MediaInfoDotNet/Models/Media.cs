6b4425f57008e7cde3b88f5ec0c06a52e46891ab
Carsten Schlote <schlote@vahanus.net>
2013-05-14 11:30:17 +0200

    [mediainfo.net] Readded lost change
    - Added value argument for Option() call.
    
    Signed-off-by: Carsten Schlote <schlote@vahanus.net>

--- MediaInfoDotNet/Models/Media.cs
+++ MediaInfoDotNet/Models/Media.cs
@@ -67,8 +67,9 @@ namespace MediaInfoDotNet.Models
 
 		/// <summary>Returns information about MediaInfo.</summary>
 		/// <param name="parameter">Option such as Info_Parameters</param>
-		public string miOption(string parameter) {
-			string miResult = mediaInfo.Option(parameter);
+		/// <param name="val">Optional argument for parameter, e.g. Opetion("Complete", "Yes") to enable debug Inform() output.</param>
+		public string miOption(string parameter, string val = "") {
+			string miResult = mediaInfo.Option(parameter, val);
 			return miResult == null ? string.Empty : miResult;
 		}
 
