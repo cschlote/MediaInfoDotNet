559467fc67f08dfc5fdd6ffbc38a7d85a02baad7
Carsten Schlote <schlote@vahanus.net>
2014-03-05 00:38:02 +0100

    nuget spec: version infos: Updated entries
    - other cleanups
    
    Signed-off-by: Carsten Schlote <schlote@vahanus.net>

--- MediaInfoDotNet/Properties/AssemblyInfo.cs
+++ MediaInfoDotNet/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("vahanus.net")]
 [assembly: AssemblyProduct("MediaInfoDotNet")]
-[assembly: AssemblyCopyright("Copyright ©  2012 Charles Burns")]
+[assembly: AssemblyCopyright("Copyright ©  2013 Carsten Schlote")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.62.40922")]
+[assembly: AssemblyVersion("0.7.67.1")]
 //[assembly: AssemblyFileVersion("0.7.*")]
