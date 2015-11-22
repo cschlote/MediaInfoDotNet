b40f17bdf1b398c53f4021dcc16fc75a9ed0136b
Carsten Schlote <schlote@vahanus.net>
2015-11-22 18:02:59 +0100

    Updated dll binaries to version 0.7.79
    Also updated nuspec and assembly info.
    
    Signed-off-by: Carsten Schlote <schlote@vahanus.net>

--- MediaInfoDotNet/Properties/AssemblyInfo.cs
+++ MediaInfoDotNet/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("vahanus.net")]
 [assembly: AssemblyProduct("MediaInfoDotNet")]
-[assembly: AssemblyCopyright("Copyright ©  2013-2015 Carsten Schlote")]
+[assembly: AssemblyCopyright("Copyright © 2013-2015 Carsten Schlote")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.67.40922")]
+[assembly: AssemblyVersion("0.7.79.151122")]
 //[assembly: AssemblyFileVersion("0.7.*")]
